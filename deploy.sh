850fe413efeda818457e921413157ae7ffb71e4a
Matthew Huie <matthew@matthewhuie.com>
2026-01-30 21:13:13 -0500

    Removing unnecessary reference to workspace

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "$INPUT_SERVICE_ACCOUNT_KEY_JSON" > /key-file.json
 export GOOGLE_APPLICATION_CREDENTIALS="/key-file.sjon"
 gcloud auth activate-service-account --key-file=/key-file.json --project "$INPUT_PROJECT_ID"
 
-adk deploy agent_engine --project=$INPUT_PROJECT_ID --region=$INPUT_REGION "$INPUT_ADK_PARAMETERS" $GITHUB_WORKSPACE/$INPUT_AGENT
+adk deploy agent_engine --project=$INPUT_PROJECT_ID --region=$INPUT_REGION "$INPUT_ADK_PARAMETERS" $INPUT_AGENT
